d55743cba07e876919185737ad9b71528c439983
axmand <815966508@qq.com>
2018-12-22 15:24:04 +0800

    Add Test Samples

--- UnitTests/UnitTest.cs
+++ UnitTests/UnitTest.cs
@@ -1,14 +1,51 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neuro.Activation;
+using Neuro.Learning;
+using Neuro.Networks;
 
 namespace UnitTests
 {
     [TestClass]
     public class UnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        double[][] inputs = new double[5][];
+
+        double[][] outputs = new double[5][];
+
+        public void InitialSamples()
         {
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                inputs[i] = new double[3] { rand.NextDouble(), rand.NextDouble(), rand.NextDouble() };
+                outputs[i] = new double[1] { i % 2 };
+            }
+        }
+
+        /// <summary>
+        /// initialization samples
+        /// </summary>
+        public UnitTest()
+        {
+            InitialSamples();
+        }
+
+        [TestMethod]
+        public void DeepNeuralNetwork()
+        {
+            //build neural network
+            IActivationFunction activefunc = new SigmoidFunction();
+            ActivationNetwork network = new ActivationNetwork(activefunc, 3, 2, 1);
+            network.Randomize();
+            BackPropagationLearning teacher = new BackPropagationLearning(network) { Momentum = 0.9, LearningRate = 0.1};
+            //train
+            double loss = 1;
+            for (int step = 0; step < 100; step++)
+                loss = teacher.RunEpoch(inputs, outputs);
+            //
+            Assert.IsTrue(loss < 1.0);
         }
     }
 }
